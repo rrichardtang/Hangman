29c755e4ec368a3c8348e1d983f6fa7c6f55610a
rrichardtang <c3u2b4e@gmail.com>
2020-02-11 17:36:23 -0800

    Create words and rules txt files with minor changes to hangman.py

--- Backend/main/hangman.py
+++ Backend/main/hangman.py
@@ -1,71 +1,57 @@
-require_explanation = input('Hello, welcome to hangman! Type y if you would like me to explain the rules or n if you '
-                            'are ready to play.')
-
-if require_explanation == 'y':
-    print('Hangman is very simple. I will think of a random word. I will give you the number of letters in my word '
-          'indicated by _s. You will then try to guess my word by trying letters one at a time. If the letter you '
-          'guessed is in my word I will fill in the corresponding blank. If not, you get one strike. You have 6 '
-          'strikes total. Good luck!'
-    )
+with open('rules.txt', 'r') as f:
+    print(f.read())
 
 
 import random
+import string
 
-
-words = ('earpiece',
-         'laptop',
-         'clock',
-         'pastry',
-         'telephone',
-         'basketball',
-         'warrior',
-         'jacket',
-         'ostrich',
-         'xylophone'
-)
+words = open('words.txt', 'r').read().split()
 
 word = random.choice(words)
-print(' _ ' * len(word))
 
+print(' _ ' * len(word))
 guessed_letters = []
 correct_letters = ['_'] * len(word)
 
 
 def pieces_of_word():
-    for index in range(len(word)):
-        original_letter = word[index]
+    for i in range(len(word)):
+        original_letter = word[i]
         if letter == original_letter:
-            print(letter)
-            correct_letters[index] = original_letter
+            print(letter, end = ' ')
+            correct_letters[i] = original_letter
         elif original_letter in correct_letters:
-            print(original_letter)
+            print(original_letter, end = ' ')
         else:
-            print('_')
+            print('_', end = ' ')
     if ''.join(correct_letters) == word:
         print('You win!')
 
 
-def add_to_list():
+def add_letter_to_guessed():
     guessed_letters.append(letter)
     print('You have guessed these letters so far: ' + str(guessed_letters))
 
 
-counter = 6
-
+counter = len(word) * 2
 
 while counter > 0 and sorted(correct_letters) != sorted(word):
-    letter = input('Guess a letter')
+    letter = input('Guess a letter').lower()
+    assert len(letter) == 1, 'You can only guess one letter at a time'
+    assert letter in string.ascii_lowercase, 'You can only guess letters'
+    
     if letter in guessed_letters:
         print('You already guessed this letter! Please try another letter')
     elif letter in word:
-        add_to_list()
+        add_letter_to_guessed()
         pieces_of_word()
     else:
-        add_to_list()
+        add_letter_to_guessed()
         pieces_of_word()
         counter -= 1
-        print('You have ' + str(counter) + ' guesses left')
+        print('You have {0} guesses left'.format(counter))
 
 
 if counter == 0:
-    print('Sorry, you did not guess the word in time!')
+    print('Sorry, you did not guess the word in time!, the word was {0}'.format\
+          (word))
